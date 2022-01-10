95176f13976803e72f3cb3d4c7e212bff103cc1b
Finnhumano24 <89821577+Finnhumano24@users.noreply.github.com>
2022-01-10 14:33:46 -0400

    Delete lolo.java

--- lolo.java
+++
@@ -1,5 +0,0 @@
-public class lolo{
-    public static void main(String args[]){
-      System.out.print("I like to eat bananas and play video games thanks for your time now you can go thanks a lot bye now if you allow me I'll stop writing") ;
-    }
-}
